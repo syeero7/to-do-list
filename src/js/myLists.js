ae1b3c32320433d28473d929c53c405ad43ca37c
syeero7 <EmalHasitha@proton.me>
2024-09-27 09:28:24 +0530

    Add functions to add todo list to the list

--- src/js/myLists.js
+++ src/js/myLists.js
@@ -1,11 +1,17 @@
 import lists from "../js/classes/lists";
+import ToDoList from "../js/classes/toDoList";
 import createListElements from "./createListEl";
 import {
   removeElements,
+  toDoListForm,
   myLists,
+  listNameInput,
+  generateNewId,
 } from "./common";
 
 export default () => {
+  toDoListForm.addEventListener("submit", getUserInput);
+
   refreshList();
 };
 
@@ -20,4 +26,25 @@ function clearList() {
 function refreshList() {
   renderLists();
   clearList();
+}
+
+function getUserInput(e) {
+  e.preventDefault();
+  const listName = listNameInput.value.trim();
+  if (!listName.length) return;
+  addToDoList(null, listName);
+}
+
+function addToDoList(listId = null, listName) {
+  if (listId === null) {
+    listId = generateNewId();
+  }
+
+  const toDoList = new ToDoList();
+  toDoList.id = listId;
+  toDoList.name = listName;
+  lists.addList(toDoList);
+
+  listNameInput.value = "";
+  refreshList()
 }
