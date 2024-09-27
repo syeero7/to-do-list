c793f4538926e7671934723c7816f1222374f4e9
syeero7 <EmalHasitha@proton.me>
2024-09-27 11:20:15 +0530

    Add functions to delete a todo list from the list

---
+++ src/js/deleteList.js
@@ -0,0 +1,27 @@
+import lists from "./classes/lists";
+import { refreshList } from "./myLists";
+import { deleteListDialog, deleteListTitle } from "./common";
+
+export default function deleteList(toDoListId) {
+  renderDeleteListTitle(toDoListId);
+
+  deleteListDialog.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.matches(".yesBtn")) {
+      lists.deleteList(toDoListId);
+      deleteListDialog.close();
+      refreshList();
+    }
+
+    if (target.matches(".noBtn")) deleteListDialog.close();
+  });
+
+  deleteListDialog.showModal();
+}
+
+function renderDeleteListTitle(toDoListId) {
+  lists.getLists().find((list) => {
+    if (list.id == toDoListId) deleteListTitle.textContent = list.name;
+  });
+}
