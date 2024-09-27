d14de36075c0bf8f07ad8e809a843a39cfc3d4d1
syeero7 <EmalHasitha@proton.me>
2024-09-27 16:37:19 +0530

    Add functions to clear and render the tasks

--- src/js/taskList.js
+++ src/js/taskList.js
@@ -1,5 +1,12 @@
 import lists from "./classes/lists";
-import { myLists, selectedList, addTaskButton } from "./common";
+import { createTaskElements } from "./createTaskEl";
+import {
+  myLists,
+  selectedList,
+  addTaskButton,
+  removeElements,
+  toDoList,
+} from "./common";
 
 export function selectList(toDoListId, targetElement) {
   myLists.querySelectorAll("p").forEach((para) => {
@@ -9,6 +16,7 @@ export function selectList(toDoListId, targetElement) {
 
   selectedList.dataset.selectedListId = toDoListId;
   resetSelectedList();
+  refreshToDoList()
 }
 
 export function resetSelectedList() {
@@ -28,4 +36,20 @@ export function resetSelectedList() {
       });
     }
   });
+}
+
+function renderTasks() {
+  const toDoListId = selectedList.dataset.selectedListId;
+
+  const list = lists.getLists().find((list) => list.id == toDoListId);
+  list.getToDoList().forEach((task) => createTaskElements(task));
+}
+
+function clearToDoList() {
+  removeElements(toDoList);
+}
+
+function refreshToDoList() {
+  clearToDoList();
+  renderTasks();
 }
